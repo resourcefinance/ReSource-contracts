698b920e18199c5836092df7a65eb0dee3763221
tavakyan <tigran.avakyan@icloud.com>
2021-02-24 12:28:32 -0800

    Adding in README and truffle-config changes for local celo ganache env

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,31 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const mnemonicPhrase = "mountains supernatural bird ...";
+
 module.exports = {
   plugins: ["solidity-coverage"],
   networks: {
     development: {
       host: "localhost",
-      port: 8545,
+      port: 7545,
       network_id: "*",
       gas: 12000000,
     },
+    test: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "1101",
+      provider: () =>
+          new HDWalletProvider({
+            mnemonic: {
+              phrase: mnemonicPhrase
+            },
+            providerOrUrl: "https://ropsten.infura.io/v3/YOUR-PROJECT-ID",
+            numberOfAddresses: 1,
+            shareNonce: true,
+            derivationPath: "m/44'/1'/0'/0/"
+          }),
+      gas: 12000000,
+    }
   },
   compilers: {
     solc: {
